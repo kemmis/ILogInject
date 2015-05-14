dacccfc19b1345a9453ced3a119cfe5eea7c8bb9
Rafe Kemmis <rafe@kemmis.info>
2015-05-14 19:43:57 -0400

    removed some stuff i don't need in the project, added my own nuget publishing setup

---
+++ src/ILogInject.Unity/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ILogInject.Unity.Updated")]
+[assembly: AssemblyDescription("An updated fork of the ILogInject.Unity project that uses the latest Common.Logging library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rafe Kemmis")]
+[assembly: AssemblyProduct("ILogInject.Unity")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59c7bb82-2b5c-4238-ab81-9bd36ea7c21f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.*")]
